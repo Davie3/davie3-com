4a7f4e9698f51de4fa150d826ca3b1e5ab911008
Davie3 <Davie3@users.noreply.github.com>
2025-09-10 16:49:55 -0700

    Revert "security: restrict unsafe-eval CSP directive to development environment only"
    This reverts commit 0ad7ad6751a0a9482f7e0fd235c465b890d99b97.

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,28 @@
 import type { NextConfig } from 'next';
 import { URL_MAPPINGS } from './src/lib/config/redirect-config';
 
-const isDevelopment = process.env.VERCEL_ENV !== 'production';
-
 const cspHeader = `
-  default-src 'self';
-  script-src 'self' ${isDevelopment ? "'unsafe-eval'" : ''}
-    static.cloudflareinsights.com challenges.cloudflare.com *.cloudflare.com
-    *.vercel-insights.com *.vercel-analytics.com https://vercel.live *.vercel.app;
-  style-src 'self' 'unsafe-inline' https://vercel.live;
-  img-src 'self' blob: data: https://vercel.live https://vercel.com;
-  font-src 'self' https://vercel.live https://assets.vercel.com;
-  object-src 'none';
-  base-uri 'self';
-  form-action 'self';
-  frame-ancestors 'none';
-  frame-src challenges.cloudflare.com *.cloudflare.com https://vercel.live;
-  connect-src 'self'
-    cloudflareinsights.com challenges.cloudflare.com *.cloudflare.com
-    *.vercel-insights.com *.vercel-analytics.com vitals.vercel-insights.com
-    https://vercel.live *.vercel.app wss://ws-us3.pusher.com;
-  upgrade-insecure-requests;
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' 
+        static.cloudflareinsights.com challenges.cloudflare.com *.cloudflare.com
+        *.vercel-insights.com *.vercel-analytics.com https://vercel.live *.vercel.app;
+    style-src 'self' 'unsafe-inline'
+        https://vercel.live;
+    img-src 'self' blob: data:
+        https://vercel.live https://vercel.com;
+    font-src 'self'
+        https://vercel.live https://assets.vercel.com;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    frame-src 'self'
+        challenges.cloudflare.com *.cloudflare.com
+        https://vercel.live;
+    connect-src 'self'
+        cloudflareinsights.com challenges.cloudflare.com *.cloudflare.com
+        *.vercel-insights.com *.vercel-analytics.com vitals.vercel-insights.com https://vercel.live *.vercel.app wss://ws-us3.pusher.com;
+    upgrade-insecure-requests;
 `;
 
 const nextConfig: NextConfig = {
